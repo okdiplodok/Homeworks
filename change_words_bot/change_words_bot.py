352491c7c167f9c5b54cfc5188b497c226265cbc
zu-ann <asl29112@gmail.com>
2017-06-03 03:01:44 +0300

    Update and rename change_words_bot.py to change_words_bot/change_words_bot.py

--- change_words_bot/change_words_bot.py
+++ change_words_bot/change_words_bot.py
@@ -20,7 +20,8 @@ morph = MorphAnalyzer()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет! Это бот, который генерирует ответы, используя слова из антиутопии Дж.Оруэлла \"1984\". Напиши что-нибудь!")
+    bot.send_message(message.chat.id, "Привет! Это бот, который генерирует ответы, используя слова из антиутопии Дж.Оруэлла \"1984\". 
+                     \Напиши что-нибудь!")
 
 def split_sentence(message):
     text = message.text
